8048855ba8cfde7b4496ce3a3f14a9cecaca5f35
Trishala Jadhav <88381436+trishala1999@users.noreply.github.com>
2022-12-23 15:24:38 +0530

    Update const.go

--- policy/const.go
+++ policy/const.go
@@ -5,25 +5,24 @@ const (
 	plural   = "policies"
 )
 
-// Valid values for Policy.Rule.RuleType.
 const (
-	RuleTypeConfig     = "Config"
-	RuleTypeAuditEvent = "AuditEvent"
-	RuleTypeNetwork    = "Network"
-	RuleTypeIAM        = "IAM"
-	RuleTypeAnomaly    = "Anomaly"
-	RuleTypeData       = "DLP"
+	RuleTypeConfig        = "Config"
+	RuleTypeAuditEvent    = "AuditEvent"
+	RuleTypeNetwork       = "Network"
+	RuleTypeIAM           = "IAM"
+	RuleTypeAnomaly       = "Anomaly"
+	RuleTypeData          = "DLP"
+	RuleTypeNetworkConfig = "NetworkConfig"
 )
 
 // Valid values for Policy.PolicyType.
 const (
-	PolicyTypeConfig      = "config"
-	PolicyTypeAuditEvent  = "audit_event"
-	PolicyTypeNetwork     = "network"
-	PolicyTypeIAM         = "iam"
-	PolicyTypeAnomaly     = "anomaly"
-	PolicyTypeData        = "data"
-	RuleTypeNetworkConfig = "NetworkConfig"
+	PolicyTypeConfig     = "config"
+	PolicyTypeAuditEvent = "audit_event"
+	PolicyTypeNetwork    = "network"
+	PolicyTypeIAM        = "iam"
+	PolicyTypeAnomaly    = "anomaly"
+	PolicyTypeData       = "data"
 )
 
 // Valid values for Policy.Rule.Severity.
