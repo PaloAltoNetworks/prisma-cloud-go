b6964aec3d746cab23868a2a0a35b00c6ac75720
UshaPriya2001 <spriya@paloaltonetworks.com>
2023-03-24 14:54:12 +0530

    Added permission group resource and data source blocks

--- error.go
+++ error.go
@@ -9,6 +9,9 @@ import (
 var InvalidCredentialsError = errors.New("invalid credentials")
 var ObjectNotFoundError = errors.New("object not found")
 var AlreadyExistsError = errors.New("object already exists")
+var AccountGroupNotFoundError = errors.New("account_group_not_found")         //account_group_not_found
+var InternalError = errors.New("internal_error")                              //compliance standard requirement
+var InvalidPermissionGroupIdError = errors.New("invalid_permission_group_id") //permission group
 
 type PrismaCloudErrorList struct {
 	Errors     []PrismaCloudError
@@ -34,10 +37,16 @@ func (e PrismaCloudErrorList) Error() string {
 
 func (e PrismaCloudErrorList) GenericError() error {
 	for i := range e.Errors {
-		if e.Errors[i].ObjectNotFound() {
+		if e.Errors[i].InvalidPermissionGroupIdError() {
+			return InvalidPermissionGroupIdError
+		} else if e.Errors[i].AccountGroupNotFoundError() {
+			return AccountGroupNotFoundError
+		} else if e.Errors[i].ObjectNotFound() {
 			return ObjectNotFoundError
 		} else if e.Errors[i].AlreadyExists() {
 			return AlreadyExistsError
+		} else if e.Errors[i].InternalError() {
+			return InternalError
 		}
 	}
 
@@ -58,9 +67,19 @@ func (e PrismaCloudError) ObjectNotFound() bool {
 
 	return false
 }
-
+func (e PrismaCloudError) InvalidPermissionGroupIdError() bool {
+	return strings.HasSuffix(e.Message, "invalid_permission_group_id")
+}
 func (e PrismaCloudError) AlreadyExists() bool {
 	return strings.HasSuffix(e.Message, "_already_exists")
+}
+
+func (e PrismaCloudError) InternalError() bool {
+	return strings.HasSuffix(e.Message, "internal_error")
+}
+
+func (e PrismaCloudError) AccountGroupNotFoundError() bool {
+	return strings.HasSuffix(e.Message, "account_group_not_found")
 }
 
 func (e PrismaCloudError) Error() string {
