029bacaddada166593da67858372fe713bf15dbe
UshaPriya2001 <spriya@paloaltonetworks.com>
2023-04-20 13:03:35 +0530

    GCP and IBM v2 account onboarding

---
+++ cloud/account-v2/gcpTemplate/structs.go
@@ -0,0 +1,21 @@
+package gcpTemplate
+
+type GcpTemplateReq struct {
+	ProjectId          string   `json:"projectId"`
+	OrgId              string   `json:"orgId"`
+	AccountType        string   `json:"accountType"`
+	AuthenticationType string   `json:"authenticationType"`
+	Name               string   `json:"name"`
+	Features           []string `json:"features"`
+	FileName           string   `json:"fileName"`
+}
+
+type GcpTemplate struct {
+	ProjectId          string   `json:"projectId"`
+	OrgId              string   `json:"orgId"`
+	AccountType        string   `json:"accountType"`
+	AuthenticationType string   `json:"authenticationType"`
+	Name               string   `json:"name"`
+	Features           []string `json:"features"`
+	FileName           string   `json:"fileName"`
+}
